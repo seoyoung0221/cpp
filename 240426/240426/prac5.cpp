dd41f638fbbaddd494d5742a84f417f98cb11b64
seoyoung0221 <jjan000221@gmail.com>
2024-04-26 15:57:53 +0900

    prac5_1

--- 240426/240426/prac5.cpp
+++ 240426/240426/prac5.cpp
@@ -8,9 +8,15 @@ int main()
 {
 	int x, y;
 	int loop_cnt = 1;
-	cout << "두 개의 자연수를 입력하세요 : ";
-	cin >> x >> y;
+	int error = 0;
 
+	// 입력 받기
+	cout << "첫번째 자연수를 입력하세요 : ";
+	cin >> x;
+	cout << "두번째 자연수를 입력하세요 : ";
+	cin >> y; 
+
+	// 동적 배열 만들기
 	int** arr = new int* [x];
 
 	for (int i = 0; i < x; i++)
@@ -27,6 +33,7 @@ int main()
 		}
 	}
 
+	// 동적 배열 출력
 	for (int i = 0; i < x; i++)
 	{
 		for (int j = 0; j < y; j++)
@@ -36,6 +43,7 @@ int main()
 		cout << "\n";
 	}
 
+	// 동적 배열 해제
 	for (int i = 0; i < x; i++) 
 	{
 		delete[] arr[i];
