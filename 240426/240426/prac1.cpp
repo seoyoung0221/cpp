97d1b3eb858f4898e484b1fddcb2b8d2445c7294
seoyoung0221 <jjan000221@gmail.com>
2024-04-26 18:57:48 +0900

    prac1_printvector

--- 240426/240426/prac1.cpp
+++ 240426/240426/prac1.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 using namespace std;
 
+void print_vector(vector<int>& vec);
+
 int main() 
 {
 	vector<int> v(5);
@@ -43,17 +45,14 @@ int main()
 	{
 		v[i] = v.at(i) * 2;
 	}
-
-	cout << "변경된 벡터를 출력합니다. \n";
-	cout << "v = {" << v.at(0) << ", " << v.at(1) << ", " << v.at(2) << ", " << v.at(3) << ", " << v.at(4) << "} \n\n";
+	print_vector(v);
 
 	//인덱스를 입력 받아 해당 인덱스에 있는 원소 제거
 	cout << "몇 번째 원소를 제거할 것인지 선택해 주세요 : ";
 	cin >> n;
 	cout << n << "번째 원소가 제거됩니다. \n";
 	v.erase(v.begin() + n - 1, v.begin() + n);
-	cout << "현재 벡터는 \n";
-	cout << "v = {" << v.at(0) << ", " << v.at(1) << ", " << v.at(2) << ", " << v.at(3) << "} \n\n";
+	print_vector(v);
 
 	//인덱스를 선택하여 해당 인덱스에 새로운 원소 추가
 	cout << "인덱스를 선택하고 원소를 추가해주세요(인덱스는 1 ~ 5만 가능) : ";
@@ -66,8 +65,24 @@ int main()
 	else {
 		v.insert(v.begin() + n - 1, value);
 	}
-	cout << "현재 벡터는 \n";
-	cout << "v = {" << v.at(0) << ", " << v.at(1) << ", " << v.at(2) << ", " << v.at(3) << ", " << v.at(4) << "} \n";
+	//cout << "현재 벡터는 \n";
+	//cout << "v = {" << v.at(0) << ", " << v.at(1) << ", " << v.at(2) << ", " << v.at(3) << ", " << v.at(4) << "} \n";
+	print_vector(v);
 
 	return 0;
+}
+
+void print_vector(vector<int>& vec) 
+{
+	cout << "현재 벡터 : " << "v = {";
+	for (int i = 0; i < vec.size(); i++)
+	{
+		if (i != vec.size() - 1)
+		{
+			cout << vec.at(i) << ", ";
+		}
+		else {
+			cout << vec.at(i) << "}\n\n";
+		}
+	}
 }
